6c4f48f86f028798553d29f6b1598b47e8c18974
Jacob Irwanto <jacobirwanto@gmail.com>
2024-05-16 14:41:30 +0700

    feat(main) initialize project structure, config, assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      fontFamily: {
+        maximus: ["Maximus"],
+      },
+      animation: {
+        fadeIn: "fadeIn 0.4s ease-in-out",
       },
     },
   },
